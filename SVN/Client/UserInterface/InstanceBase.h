e2d977a98973ffd79f4ca186d3a54d17b1682702
Licymnius <licymnius@outlook.com>
2025-08-21 12:48:32 +0300

    Create InstanceBase.h

---
+++ SVN/Client/UserInterface/InstanceBase.h
@@ -0,0 +1,27 @@
+//find;
+
+		AFFECT_NUM = 64,
+
+//add above;
+
+#ifdef AFFECT_SYSTEM
+		AFFECT_YELLOW,// xx
+		AFFECT_BROWN,// xx
+		AFFECT_GREY,// xx
+		AFFECT_ORANGE,// xx
+		AFFECT_PINK,// xx
+		AFFECT_PURPLE,// xx
+		AFFECT_TURQUOISE,// xx
+		AFFECT_BLUE,// xx
+		AFFECT_RED,// xx
+		AFFECT_ELR,// xx
+		AFFECT_ELY,// xx
+		AFFECT_ELG,// xx
+		AFFECT_ELB,// xx
+		AFFECT_ELP,// xx
+		AFFECT_ELW,// xx
+		AFFECT_TORY,// xx
+		AFFECT_TORB,// xx
+		AFFECT_TORR,// xx
+		AFFECT_TORG,// xx
+#endif
