da74985ca99cf794ded4b95a44da8a9687127aa9
Licymnius <licymnius@outlook.com>
2025-08-21 12:33:31 +0300

    Create playersettingmodule.py

---
+++ Client/root/playersettingmodule.py
@@ -0,0 +1,27 @@
+#find;
+
+	chrmgr.RegisterEffect(chrmgr.EFFECT_REFINED+1, "PART_WEAPON", "D:/ymir work/pc/common/effect/sword/sword_7.mse")
+
+#add above;
+
+	if app.AFFECT_SYSTEM:
+    #You need to edit the xx part according to your own in all subsequent lines.
+    chrmgr.RegisterEffect(chrmgr.EFFECT_AFFECT+xx, "Bip01", "d:/ymir work/pc/common/effect/armor/desert_yellow.mse")# default: 47
+		chrmgr.RegisterEffect(chrmgr.EFFECT_AFFECT+xx, "Bip01", "d:/ymir work/pc/common/effect/armor/desert_brown.mse")# default: 48
+		chrmgr.RegisterEffect(chrmgr.EFFECT_AFFECT+xx, "Bip01", "d:/ymir work/pc/common/effect/armor/desert_gray.mse")# default: 49
+		chrmgr.RegisterEffect(chrmgr.EFFECT_AFFECT+xx, "Bip01", "d:/ymir work/pc/common/effect/armor/desert_orange.mse")# default: 50
+		chrmgr.RegisterEffect(chrmgr.EFFECT_AFFECT+xx, "Bip01", "d:/ymir work/pc/common/effect/armor/desert_pink.mse")# default: 51
+		chrmgr.RegisterEffect(chrmgr.EFFECT_AFFECT+xx, "Bip01", "d:/ymir work/pc/common/effect/armor/desert_purple.mse")# default: 52
+		chrmgr.RegisterEffect(chrmgr.EFFECT_AFFECT+xx, "Bip01", "d:/ymir work/pc/common/effect/armor/desert_turquoise.mse")# default: 53
+		chrmgr.RegisterEffect(chrmgr.EFFECT_AFFECT+xx, "Bip01", "d:/ymir work/pc/common/effect/armor/desert_blue.mse")# default: 54
+		chrmgr.RegisterEffect(chrmgr.EFFECT_AFFECT+xx, "Bip01", "d:/ymir work/pc/common/effect/armor/desert_red.mse")# default: 55
+		chrmgr.RegisterEffect(chrmgr.EFFECT_AFFECT+xx, "Bip01", "d:/ymir work/pc/common/effect/armor/red_electric.mse")# default: 56
+		chrmgr.RegisterEffect(chrmgr.EFFECT_AFFECT+xx, "Bip01", "d:/ymir work/pc/common/effect/armor/yellow_electric.mse")# default: 57
+		chrmgr.RegisterEffect(chrmgr.EFFECT_AFFECT+xx, "Bip01", "d:/ymir work/pc/common/effect/armor/green_electric.mse")# default: 58
+		chrmgr.RegisterEffect(chrmgr.EFFECT_AFFECT+xx, "Bip01", "d:/ymir work/pc/common/effect/armor/blue_electric.mse")# default: 59
+		chrmgr.RegisterEffect(chrmgr.EFFECT_AFFECT+xx, "Bip01", "d:/ymir work/pc/common/effect/armor/pink_electric.mse")# default: 60
+		chrmgr.RegisterEffect(chrmgr.EFFECT_AFFECT+xx, "Bip01", "d:/ymir work/pc/common/effect/armor/white_electric.mse")# default: 61
+		chrmgr.RegisterEffect(chrmgr.EFFECT_AFFECT+xx, "Bip01", "d:/ymir work/pc/common/effect/armor/tornado_yellow.mse")# default: 62
+		chrmgr.RegisterEffect(chrmgr.EFFECT_AFFECT+xx, "Bip01", "d:/ymir work/pc/common/effect/armor/tornado_blue.mse")# default: 63
+		chrmgr.RegisterEffect(chrmgr.EFFECT_AFFECT+xx, "Bip01", "d:/ymir work/pc/common/effect/armor/tornado_red.mse")# default: 64
+		chrmgr.RegisterEffect(chrmgr.EFFECT_AFFECT+xx, "Bip01", "d:/ymir work/pc/common/effect/armor/tornado_green.mse")# default: 65
