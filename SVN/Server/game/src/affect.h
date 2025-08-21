ce7a1acef11fd8da625a74e2e9a0f382a42a1065
Licymnius <licymnius@outlook.com>
2025-08-21 12:40:33 +0300

    Create affect.h

---
+++ SVN/Server/game/src/affect.h
@@ -0,0 +1,56 @@
+//find;
+
+	AFFECT_RAMADAN_ABILITY = 300,
+
+//add above;
+
+#ifdef AFFECT_SYSTEM
+//You need to edit the xxx part according to your own in all subsequent lines.You need to edit the xx part according to your own in all subsequent lines.
+  AFFECT_YELLOW = xxx,
+	AFFECT_BROWN = xxx,
+	AFFECT_GREY = xxx,
+	AFFECT_ORANGE = xxx,
+	AFFECT_PINK = xxx,
+	AFFECT_PURPLE = xxx,
+	AFFECT_TURQUOISE = xxx,
+	AFFECT_BLUE = xxx,
+	AFFECT_RED = xxx,
+	AFFECT_ELR = xxx,
+	AFFECT_ELY = xxx,
+	AFFECT_ELG = xxx,
+	AFFECT_ELB = xxx,
+	AFFECT_ELP = xxx,
+	AFFECT_ELW = xxx,
+	AFFECT_TORY = xxx,
+	AFFECT_TORB = xxx,
+	AFFECT_TORR = xxx,
+	AFFECT_TORG = xxx,
+#endif
+
+//find again;
+
+	AFF_BITS_MAX
+
+//add above;
+
+#ifdef AFFECT_SYSTEM
+	AFF_YELLOW, // xx
+	AFF_BROWN, // xx
+	AFF_GREY, // xx
+	AFF_ORANGE, // xx
+	AFF_PINK, // xx
+	AFF_PURPLE, // xx
+	AFF_TURQUOISE, // xx
+	AFF_BLUE, // xx
+	AFF_RED, // xx
+	AFF_ELR, // xx
+	AFF_ELY, // xx
+	AFF_ELG, // xx
+	AFF_ELB, // xx
+	AFF_ELP, // xx
+	AFF_ELW, // xx
+	AFF_TORY, // xx
+	AFF_TORB, // xx
+	AFF_TORR, // xx
+	AFF_TORG, // xx
+#endif
